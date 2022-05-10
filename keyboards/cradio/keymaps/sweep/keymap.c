3f037e340b3d030156f662c6e3f56c95a473f854
Jasper van Thuijl <jaspervanthuijl@gmail.com>
2022-05-10 10:34:03 +0200

    Remove mouse layer

--- keyboards/cradio/keymaps/sweep/keymap.c
+++ keyboards/cradio/keymaps/sweep/keymap.c
@@ -82,13 +82,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TD_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     ALT_T(KC_A),GUI_T(KC_S),CTL_T(KC_D),SFT_T(KC_F), KC_G,    KC_H, SFT_T(KC_J),CTL_T(KC_K),GUI_T(KC_L),ALT_T(KC_SCLN),
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          LT(2,KC_TAB), KC_SFTENT,      KC_SPC,  LT(1,KC_BSPC)
+                          LT(2,KC_TAB), KC_SFTENT,   KC_SPC,  LT(1,KC_BSPC)
     ),
     [1] = LAYOUT_split_3x5_2(
-    KC_DOT, KC_1,    KC_2,    KC_3,    KC_VOLU,     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DQUO,
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_VOLU,     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DQUO,
     KC_PSCR, KC_4,    KC_5,    KC_6,    KC_VOLD,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
     KC_CAPS, KC_7,    KC_8,    KC_9,    KC_0,        _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,
-                               MO(3),   KC_GESC,     _______, _______
+                               MO(3),   KC_ESC,      _______, _______
     ),
     [2] = LAYOUT_split_3x5_2(
     KC_LEAD, KC_LBRC, KC_LCBR, KC_RCBR, _______,     KC_CIRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_TILD,
@@ -97,9 +97,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                _______, _______,     KC_DEL,  MO(3)
     ),
     [3] = LAYOUT_split_3x5_2(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F10,      _______, KC_WH_U, KC_WH_D, _______, RESET,
-    _______, KC_F4,   KC_F5,   KC_F6,   KC_F11,      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_INS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F12,      _______, KC_BTN1, KC_BTN2, _______, _______,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F10,      _______, _______, _______, _______, RESET,
+    _______, KC_F4,   KC_F5,   KC_F6,   KC_F11,      _______, KC_CUT, KC_COPY, KC_PASTE, _______, 
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F12,      _______, _______, _______, _______, _______,
                                _______, _______,     _______, _______
     )
 };
